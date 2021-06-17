260a9ee7e6ef881984f9b81f72b58a76bae70943
Matheus Ferreira <matheus_fsilva05@hotmail.com>
2021-06-17 09:59:47 -0300

    pagina-detalhe-livro

--- src/main/kotlin/br/com/zupacademy/matheus/casadocodigo/livro/ListaLivroController.kt
+++ src/main/kotlin/br/com/zupacademy/matheus/casadocodigo/livro/ListaLivroController.kt
@@ -1,13 +1,21 @@
 package br.com.zupacademy.matheus.casadocodigo.livro
 
+import br.com.zupacademy.matheus.casadocodigo.autor.Autor
+import br.com.zupacademy.matheus.casadocodigo.categoria.Categoria
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import java.math.BigDecimal
+import java.time.LocalDate
+import javax.persistence.Column
+import javax.persistence.ManyToOne
 
 
 @RestController
@@ -23,9 +31,32 @@ class ListaLivroController(val livroRepository: LivroRepository) {
             .also { log.info("Listando todos livros") }
             .let { return ResponseEntity.ok(it) }
     }
+
+    @GetMapping("/{id}")
+    fun listarPorId(@PathVariable id: Long): ResponseEntity<DetalhesLivroResponse> {
+        return livroRepository.findById(id)
+            .map(::DetalhesLivroResponse)
+            .map { livro -> ResponseEntity.ok(livro)}
+            .orElseGet{
+                log.warn("Id informado não encontrado")
+                ResponseEntity.notFound().build()
+            }
+    }
 }
 
 class LivroResponse(livro: Livro) {
     val id = livro.id
     val nome = livro.titulo
+}
+
+class DetalhesLivroResponse(livro: Livro) {
+    val titulo = livro.titulo
+    val resumo = livro.resumo
+    val sumario = livro.sumario
+    val preco = livro.preco
+    val paginas = livro.paginas
+    val isbn = livro.isbn
+    val dataLancamento = livro.dataLancamento
+    val categoria = livro.categoria
+    val autor = livro.autor
 }
